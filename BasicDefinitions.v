9566a5249f748a499d92dd875980b68f340a9fc4
Ilmārs Cīrulis <56280933+LessnessRandomness@users.noreply.github.com>
2024-07-04 20:59:33 +0300

    Modified first three files for square boards
    Stalemates.v and Checkmates.v postponed

--- BasicDefinitions.v
+++ BasicDefinitions.v
@@ -3,52 +3,51 @@ Set Implicit Arguments.
 Definition White := 0.
 Definition Black := 1.
 
-Structure Position (Nx Ny: nat) := {
+Structure Position N := {
   WKx: nat;
   WKy: nat;
   WRx: nat;
   WRy: nat;
   BKx: nat;
   BKy: nat;
-  DimensionCheck: WKx < Nx /\ WRx < Nx /\ BKx < Nx /\ WKy < Ny /\ WRy < Ny /\ BKy < Ny;
+  DimensionCheck: WKx < N /\ WRx < N /\ BKx < N /\ WKy < N /\ WRy < N /\ BKy < N;
   Turn: nat;
 }.
 
-Definition SamePosition Nx Ny (P1 P2: Position Nx Ny) :=
+Definition SamePosition N (P1 P2: Position N) :=
   WKx P1 = WKx P2 /\ WKy P1 = WKy P2 /\
   WRx P1 = WRx P2 /\ WRy P1 = WRy P2 /\
   BKx P1 = BKx P2 /\ BKy P1 = BKy P2 /\
   Turn P1 = Turn P2.
 
-Definition NotOnSameSquare Nx Ny (P: Position Nx Ny) :=
+Definition NotOnSameSquare N (P: Position N) :=
   (WKx P <> WRx P \/ WKy P <> WRy P) /\ (Turn P = Black -> (WRx P <> BKx P \/ WRy P <> BKy P)).
 
-Definition NotKingNextToKing Nx Ny (P: Position Nx Ny) :=
+Definition NotKingNextToKing N (P: Position N) :=
   WKx P > BKx P + 1 \/ BKx P > WKx P + 1 \/ WKy P > BKy P + 1 \/ BKy P > WKy P + 1.
 
 Definition Between x1 x2 x :=
   x1 < x < x2 \/ x2 < x < x1.
 
-Definition BlackKingAttacked Nx Ny (P: Position Nx Ny) :=
+Definition BlackKingAttacked N (P: Position N) :=
   WRx P = BKx P /\ (WKx P <> WRx P \/ ~ Between (WRy P) (BKy P) (WKy P)) \/
   WRy P = BKy P /\ (WKy P <> WRy P \/ ~ Between (WRx P) (BKx P) (WKx P)).
 
-Definition LegalPosition Nx Ny (P: Position Nx Ny) :=
+Definition LegalPosition N (P: Position N) :=
   NotOnSameSquare P /\
   NotKingNextToKing P /\
   Turn P <= 1 /\
   ~ (BlackKingAttacked P /\ Turn P = White).
 
-Definition MoveWhiteKing Nx Ny (P1 P2: Position Nx Ny) :=
+Definition MoveWhiteKing N (P1 P2: Position N) :=
   WKx P1 <= WKx P2 + 1 /\ WKx P2 <= WKx P1 + 1 /\
   WKy P1 <= WKy P2 + 1 /\ WKy P2 <= WKy P1 + 1 /\
   (WKx P1 <> WKx P2 \/ WKy P1 <> WKy P2).
 
-Definition OtherAfterMoveWhiteKing Nx Ny (P1 P2: Position Nx Ny) :=
-  BKx P2 = BKx P1 /\ BKy P2 = BKy P1 /\
-  WRx P2 = WRx P1 /\ WRy P2 = WRy P1.
+Definition OtherAfterMoveWhiteKing N (P1 P2: Position N) :=
+  BKx P2 = BKx P1 /\ BKy P2 = BKy P1 /\ WRx P2 = WRx P1 /\ WRy P2 = WRy P1.
 
-Definition LegalMoveWhiteKing Nx Ny (P1 P2: Position Nx Ny) :=
+Definition LegalMoveWhiteKing N (P1 P2: Position N) :=
   MoveWhiteKing P1 P2 /\
   OtherAfterMoveWhiteKing P1 P2 /\
   Turn P1 = White /\
@@ -56,7 +55,7 @@ Definition LegalMoveWhiteKing Nx Ny (P1 P2: Position Nx Ny) :=
   LegalPosition P1 /\
   LegalPosition P2.
 
-Definition MoveWhiteRook Nx Ny (P1 P2: Position Nx Ny) :=
+Definition MoveWhiteRook N (P1 P2: Position N) :=
   (WRx P1 = WRx P2 /\ WRy P1 <> WRy P2 /\
    (WKx P1 <> WRx P1 \/ ~ Between (WRy P1) (WRy P2) (WKy P1)) /\
    (BKx P1 <> WRx P1 \/ ~ Between (WRy P1) (WRy P2) (BKy P1)))
@@ -65,11 +64,10 @@ Definition MoveWhiteRook Nx Ny (P1 P2: Position Nx Ny) :=
    (WKy P1 <> WRy P1 \/ ~ Between (WRx P1) (WRx P2) (WKx P1)) /\
    (BKy P1 <> WRy P1 \/ ~ Between (WRx P1) (WRx P2) (BKx P1))).
 
-Definition OtherAfterMoveWhiteRook Nx Ny (P1 P2: Position Nx Ny) :=
-  WKx P2 = WKx P1 /\ WKy P2 = WKy P1 /\
-  BKx P2 = BKx P1 /\ BKy P2 = BKy P1.
+Definition OtherAfterMoveWhiteRook N (P1 P2: Position N) :=
+  WKx P2 = WKx P1 /\ WKy P2 = WKy P1 /\ BKx P2 = BKx P1 /\ BKy P2 = BKy P1.
 
-Definition LegalMoveWhiteRook Nx Ny (P1 P2: Position Nx Ny) :=
+Definition LegalMoveWhiteRook N (P1 P2: Position N) :=
   MoveWhiteRook P1 P2 /\
   OtherAfterMoveWhiteRook P1 P2 /\
   Turn P1 = White /\
@@ -77,19 +75,18 @@ Definition LegalMoveWhiteRook Nx Ny (P1 P2: Position Nx Ny) :=
   LegalPosition P1 /\
   LegalPosition P2.
 
-Definition LegalWhiteMove Nx Ny (P1 P2: Position Nx Ny) :=
+Definition LegalWhiteMove N (P1 P2: Position N) :=
   LegalMoveWhiteKing P1 P2 \/ LegalMoveWhiteRook P1 P2.
 
-Definition MoveBlackKing Nx Ny (P1 P2: Position Nx Ny) :=
+Definition MoveBlackKing N (P1 P2: Position N) :=
   BKx P1 <= BKx P2 + 1 /\ BKx P2 <= BKx P1 + 1 /\
   BKy P1 <= BKy P2 + 1 /\ BKy P2 <= BKy P1 + 1 /\
   (BKx P1 <> BKx P2 \/ BKy P1 <> BKy P2).
 
-Definition OtherAfterMoveBlackKing Nx Ny (P1 P2: Position Nx Ny) :=
-  WKx P2 = WKx P1 /\ WKy P2 = WKy P1 /\
-  WRx P2 = WRx P1 /\ WRy P2 = WRy P1.
+Definition OtherAfterMoveBlackKing N (P1 P2: Position N) :=
+  WKx P2 = WKx P1 /\ WKy P2 = WKy P1 /\ WRx P2 = WRx P1 /\ WRy P2 = WRy P1.
 
-Definition LegalBlackMove Nx Ny (P1 P2: Position Nx Ny) :=
+Definition LegalBlackMove N (P1 P2: Position N) :=
   MoveBlackKing P1 P2 /\
   OtherAfterMoveBlackKing P1 P2 /\
   Turn P1 = Black /\
@@ -97,22 +94,21 @@ Definition LegalBlackMove Nx Ny (P1 P2: Position Nx Ny) :=
   LegalPosition P1 /\
   LegalPosition P2.
 
-Definition Mate Nx Ny (P: Position Nx Ny) :=
-  LegalPosition P /\ BlackKingAttacked P /\ Turn P = Black /\ forall P': Position Nx Ny, ~ LegalBlackMove P P'.
+Definition Checkmate N (P: Position N) :=
+  LegalPosition P /\ BlackKingAttacked P /\ Turn P = Black /\ forall P': Position N, ~ LegalBlackMove P P'.
 
-Definition Stalemate Nx Ny (P: Position Nx Ny) :=
-  LegalPosition P /\ ~ BlackKingAttacked P /\ Turn P = Black /\ forall P': Position Nx Ny, ~ LegalBlackMove P P'.
+Definition Stalemate N (P: Position N) :=
+  LegalPosition P /\ ~ BlackKingAttacked P /\ Turn P = Black /\ forall P': Position N, ~ LegalBlackMove P P'.
 
-Definition InsufficientMaterial Nx Ny (P: Position Nx Ny) :=
+Definition InsufficientMaterial N (P: Position N) :=
   LegalPosition P /\ Turn P = White /\ BKx P = WRx P /\ BKy P = WRy P.
 
-Definition Draw Nx Ny (P: Position Nx Ny) := InsufficientMaterial P \/ Stalemate P.
+Definition Draw N (P: Position N) := InsufficientMaterial P \/ Stalemate P.
 
-
-Inductive BlackLoss Nx Ny: Position Nx Ny -> nat -> Prop :=
-  | black_lost: forall P, Mate P -> BlackLoss P 0
+Inductive BlackLoss N: Position N -> nat -> Prop :=
+  | black_lost: forall P, Checkmate P -> BlackLoss P 0
   | black_lost_S: forall P n, (forall P', LegalBlackMove P P' -> exists m, WhiteWin P' m /\ m <= n) ->
                   (exists P', LegalBlackMove P P' /\ WhiteWin P' n) -> BlackLoss P (S n)
 with
-WhiteWin Nx Ny: Position Nx Ny -> nat -> Prop :=
+WhiteWin N: Position N -> nat -> Prop :=
   | white_win: forall P n, (exists P', LegalWhiteMove P P' /\ BlackLoss P' n) -> WhiteWin P (S n).
--- Tactics.v
+++ Tactics.v
@@ -9,21 +9,21 @@ Ltac intuition' := intuition;
                    try (unfold MoveBlackKing in *; simpl in *; lia);
                    try (unfold OtherAfterMoveBlackKing in *; simpl in *; lia).
 
-Ltac BK_move H Nx Ny P x y :=
+Ltac BK_move H N P x y :=
   try (
-  assert (WKx P < Nx /\ WRx P < Nx /\ x < Nx /\ WKy P < Ny /\ WRy P < Ny /\ y < Ny) as DC
+  assert (WKx P < N /\ WRx P < N /\ x < N /\ WKy P < N /\ WRy P < N /\ y < N) as DC
     by (abstract (destruct P; simpl in *; lia));
   refine (H (@Build_Position _ _ (WKx P) (WKy P) (WRx P) (WRy P) x y DC White) _);
   unfold LegalBlackMove, LegalPosition in *; intuition').
 
-Ltac BK_moves H Nx Ny P :=
+Ltac BK_moves H N P :=
   try (exfalso;
-  try (BK_move H Nx Ny P (BKx P + 1) (BKy P + 1); fail);
-  try (BK_move H Nx Ny P (BKx P + 1) (BKy P); fail);
-  try (BK_move H Nx Ny P (BKx P + 1) (BKy P - 1); fail);
-  try (BK_move H Nx Ny P (BKx P) (BKy P - 1); fail);
-  try (BK_move H Nx Ny P (BKx P - 1) (BKy P - 1); fail);
-  try (BK_move H Nx Ny P (BKx P - 1) (BKy P); fail);
-  try (BK_move H Nx Ny P (BKx P - 1) (BKy P + 1); fail);
-  try (BK_move H Nx Ny P (BKx P) (BKy P + 1); fail);
+       try (BK_move H N P (BKx P + 1) (BKy P + 1); fail);
+       try (BK_move H N P (BKx P + 1) (BKy P); fail);
+       try (BK_move H N P (BKx P + 1) (BKy P - 1); fail);
+       try (BK_move H N P (BKx P) (BKy P - 1); fail);
+       try (BK_move H N P (BKx P - 1) (BKy P - 1); fail);
+       try (BK_move H N P (BKx P - 1) (BKy P); fail);
+       try (BK_move H N P (BKx P - 1) (BKy P + 1); fail);
+       try (BK_move H N P (BKx P) (BKy P + 1); fail);
   fail).
